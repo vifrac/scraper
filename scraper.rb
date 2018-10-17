77bf0eee5783e4d40d009d82d981fc81b020b951
vifrac <vifrac@gmail.com>
2018-10-17 13:50:41 -0500

    first proof with scraper

---
+++ scraper.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scraper
+  url = 'https://blockwork.cc/'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  byebug
+end
+
+scraper
